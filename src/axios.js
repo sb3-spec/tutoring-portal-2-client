6cd0e19baf71165653a3ef2f8147d53824e0a8b3
Robert Sheffield <sheffira2@gmail.com>
2022-09-12 00:42:47 -0700

    getting client ready for prod

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+require('dotenv').config(); 
+
 
 export const api = axios.create({
     baseURL: process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_URL : 'localhost:5000/',
