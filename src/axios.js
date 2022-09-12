2bc94a17e4ccfe67f633ceb58719ab37d7b6f0e7
Robert Sheffield <sheffira2@gmail.com>
2022-09-12 00:39:39 -0700

    TS

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5000/',
+    baseURL: process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_URL : 'localhost:5000/',
     headers: {
         'Content-Type': 'application/json',        
         // 'withCredentials': 'true'      
